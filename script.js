986aab636004607c21438f300102dbc08f106014
Prochezinha <nicolly.proche.nascimento@escola.pr.gov.br>
2025-09-11 08:41:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.addEventlistener('DOMcontentLoaded'), function () {
     return new Intl.NumberFormat('pt-BR',{ style: 'currency', currency:
     'BRL' }). format(valor);
     }
+ 
  // normaliza entrada (troca vírgula por ponto e converte para número )
  function toNumber(vaL) {
    if (typeof vaL === 'number') return vaL;
@@ -13,4 +14,12 @@ document.addEventlistener('DOMcontentLoaded'), function () {
  const form = document.getElementById('form');
  const erro = document.getElementById('erro');
  const resultados = document.getElementById('resultados');
- 
+ const tabelaSecao = document.getElementById('tabelaSecao');
+
+ const outPrecoComDesconto =-
+  document.getElementById('precoComDesconto');
+ const outValorParcela = docuemnt.getElementById('valorParcela');
+ const outTotalPagar = document.getElementById('TotalPagar');
+ const outEconomia = document.getElementById('economia');
+
+ if (!form) {
